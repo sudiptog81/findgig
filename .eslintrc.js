92fc6e63d12655352be8efcf9dc7063d7477d6a9
Sudipto <sudiptog81@gmail.com>
2018-12-14 12:44:28 +0530

    build check

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,10 @@ module.exports = {
     es6: true,
     node: true
   },
-  extends: "eslint:recommended",
+  extends: ["eslint:recommended", "plugin:jest/recommended"],
+  env: {
+    node: true
+  },
   parserOptions: {
     ecmaVersion: 2018
   },
