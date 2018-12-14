df358207a6d85bfcf21088485db47696b0dda2c3
Sudipto <sudiptog81@gmail.com>
2018-12-14 16:41:53 +0530

    test for ci

--- testci.js
+++ testci.js
@@ -1,3 +1,15 @@
+/* eslint-disable no-unreachable */
+const db = require("./config/database");
+
 module.exports = function hello() {
+  db.authenticate()
+    .then(() => {
+      return "Passed";
+      db.close();
+    })
+    .catch(err => {
+      return `Error: ${err}`;
+    });
+  db.close();
   return "Passed";
 };
